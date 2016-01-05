4cf27f4a2ec3c61ed3473286c0e1bd69edfca84c
Duncan Robertson <duncan@kyan.com>
2016-01-05 11:56:22 +0000

    suppress Heroku webserver errors

--- bot.js
+++ bot.js
@@ -1,12 +1,8 @@
-// Slack api token
-if (!process.env.SLACK_TOKEN) {
-  console.log('Error: missing SLACK_TOKEN environment variable.');
-  process.exit(1);
-}
+var Botkit = require('botkit');
 
-// Redis connection string
-if (!process.env.REDIS_URL) {
-  console.log('Error: missing REDIS_URL environment variable.');
+// Check for ENV's
+if (!process.env.SLACK_TOKEN || !process.env.REDIS_URL || !process.env.PORT) {
+  console.log('Error: Specify SLACK_TOKEN, REDIS_URL and port in environment');
   process.exit(1);
 }
 
@@ -16,7 +12,6 @@ var redis_storage = require('./lib/storage/redis_storage')({
 });
 
 // Bot
-var Botkit = require('botkit');
 var controller = Botkit.slackbot({
  debug: true,
  log: true,
@@ -35,4 +30,9 @@ controller.spawn({
 // Simple listener (reply to IM containing hello or hi.)
 controller.hears(['hello','hi','yo'],'direct_message', function(bot,message) {
   bot.reply(message,"Hello from BRH.");
+});
+
+// Needed to stop Heroku bailing
+controller.setupWebserver(process.env.PORT,function(err,webserver) {
+  // do nothing yet
 });
